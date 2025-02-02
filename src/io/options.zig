31007350b70a5f2620b3f0aca02863bdee001b26
nikneym <halildrk@gmail.com>
2025-02-02 13:21:20 +0300

    feat: deprecate `buffer_pool_mode` option

--- src/io/options.zig
+++ src/io/options.zig
@@ -42,22 +42,6 @@ pub const Options = struct {
         ///
         /// TODO: Support `io_uring_prep_socket`, `io_uring_prep_open` etc. functions for direct descriptors.
         direct_descriptors_mode: bool = false,
-
-        /// Experimental.
-        ///
-        /// Whether or not ring mapped provided buffers of io_uring be used.
-        ///
-        /// io_uring supports a mechanism called provided buffers. The application sets
-        /// aside a pool of buffers, and informs io_uring of those buffers. This allows the kernel to
-        /// pick a suitable buffer when the given receive operation is ready to actually receive data,
-        /// rather than upfront. The CQE posted will then additionally hold information about
-        /// which buffer was picked.
-        /// https://kernel.dk/io_uring%20and%20networking%20in%202023.pdf
-        ///
-        /// NOTE: Only available for `recv` operations at the moment.
-        /// NOTE: `recv` operations are done in multishot fashion when this mode is on.
-        /// TODO: This can be supported on Windows too via userland implementation, there's also RIO (Registered I/O) which might help.
-        buffer_pool_mode: bool = false,
     } = .{},
     // TODO:
     iocp: struct {} = .{},
