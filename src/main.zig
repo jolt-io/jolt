505cca223714ebc90e9e42b64dff42218dc037e1
nikneym <halildrk@gmail.com>
2025-02-09 13:19:04 +0300

    chore: test recv buffer pool variant

--- src/main.zig
+++ src/main.zig
@@ -17,9 +17,35 @@ const DefaultLoop = Loop(.{
 
 const Completion = DefaultLoop.Completion;
 
+//fn onRequest(userdata: *UserData, ctx: *Blitz.HttpContext) !void {
+//    const body = try ctx.parseBody(.json);
+//
+//    ctx.writeHead(.ok, &.{
+//        .{ .key = "Content-Type", .value = "text/plain" },
+//    });
+//
+//    // size of a single header
+//    // header_key_length + 2 (: ) + header_value_length + 2 (\r\n)
+//
+//    // has to be in order currently
+//    ctx.writeHeader("Connection", "keep-alive");
+//
+//    // can't send headers from now on
+//    ctx.writeBytes("hello, world!");
+//    ctx.writeBytes("Goodbye!");
+//}
+
 pub fn main() !void {
     var loop = try DefaultLoop.init();
     defer loop.deinit();
+
+    //try Blitz(UserData, .{
+    //    .read_buffer_size = 4096,
+    //    .write_buffer_size = 4096,
+    //    .on_request = onRequest,
+    //})
+    //    .init(userdata)
+    //    .run(try std.net.Address.initIp4(.{ 127, 0, 0, 1 }, 8080));
 
     // get file descriptor limits
     const rlimit = try posix.getrlimit(.NOFILE);
@@ -40,15 +66,19 @@ pub fn main() !void {
 
     const server_in_kernel: linux.fd_t = 0;
 
+    // create a buffer pool for multishot receives
+    var pool = try BufferPool.init(allocator, loop, 0, 4096, 4);
+    defer pool.deinit(allocator, loop);
+
     var accept_c = Completion{};
-    loop.accept(&accept_c, Completion, &accept_c, server_in_kernel, onAccept);
+    loop.accept(&accept_c, BufferPool, &pool, server_in_kernel, onAccept);
 
     std.debug.print("Listening on :8080\n", .{});
     try loop.run();
 }
 
 fn onAccept(
-    _: *Completion,
+    pool: *BufferPool,
     loop: *DefaultLoop,
     _: *Completion,
     result: Completion.OperationType.returnType(.accept),
@@ -56,9 +86,30 @@ fn onAccept(
     const client = result catch |err| @panic(@errorName(err));
 
     const c = allocator.create(Completion) catch unreachable;
-    const buffer = allocator.dupeZ(u8, "hey there!") catch unreachable;
 
-    loop.send(.unlinked, c, Completion, c, client, buffer, onSend);
+    loop.recvBufferPool(c, BufferPool, pool, client, pool, onRecvBp);
+    //loop.send(.unlinked, c, Completion, c, client, buffer, onSend);
+}
+
+fn onRecvBp(
+    _: *BufferPool,
+    loop: *DefaultLoop,
+    completion: *Completion,
+    socket: DefaultLoop.Socket,
+    buffer_pool: *BufferPool,
+    buffer_id: u16,
+    result: DefaultLoop.RecvBufferPoolError!usize,
+) void {
+    _ = loop;
+    _ = completion;
+    _ = socket;
+
+    const len = result catch |err| @panic(@errorName(err));
+
+    const buffer = buffer_pool.get(buffer_id);
+    defer buffer_pool.put(buffer_id);
+
+    std.debug.print("{}\n{s}\n", .{ buffer_id, buffer[0..len] });
 }
 
 fn onSend(
