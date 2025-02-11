299e682093d73ce98b017b0ba1b6f66f45ba0fac
nikneym <halildrk@gmail.com>
2025-02-11 23:33:11 +0300

    feat: expose jolt as a zig module

--- src/main.zig
+++
@@ -1,9 +0,0 @@
-const std = @import("std");
-const builtin = @import("builtin");
-
-test {
-    _ = switch (builtin.os.tag) {
-        .linux => @import("io/io_uring.zig"),
-        inline else => @panic("OS is not supported"),
-    };
-}
